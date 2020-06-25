822446c6bff858397bcd99ca7edd66d8f395bd9d
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2020-06-25 10:52:55 +0530

    Modified app.js for deployment

--- app.js
+++ app.js
@@ -78,5 +78,5 @@ app.get('*', (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('Server is up on port 3000');
+  console.log(`Server is up on port ${port}`);
 });
