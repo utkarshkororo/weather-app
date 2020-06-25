b11dd68764c46be9ab0b6ad60b173ab493b99ef7
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2020-06-25 10:19:52 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -76,6 +76,7 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen('3000', () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log('Server is up on port 3000');
 });
